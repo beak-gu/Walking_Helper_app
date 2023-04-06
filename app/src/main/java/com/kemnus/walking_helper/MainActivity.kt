3d4002a0a97bd4341ab64d199d5ea0acef83977d
beak-gu <ngw77@naver.com>
2023-04-06 17:56:55 +0900

    kemnus

--- app/src/main/java/com/kemnus/walking_helper/MainActivity.kt
+++ app/src/main/java/com/kemnus/walking_helper/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.kemnus.walking_helper
 
-import androidx.appcompat.app.AppCompatActivity
+import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Gravity
+import android.view.View
+import android.widget.*
+import com.takusemba.rtmppublisher.Publisher
+import com.takusemba.rtmppublisher.PublisherListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/kemnus/walking_helper/httpmanager.java
@@ -0,0 +1,11 @@
+package com.kemnus.walking_helper;
+
+import org.json.JSONObject;
+
+import javax.xml.transform.ErrorListener;
+
+public class httpmanager {
+    public void requestHttp(int method, String url, final JSONObject json, Listener<JSONObject> listener, ErrorListener errorListener, String tag){
+        Logger
+    }
+}
